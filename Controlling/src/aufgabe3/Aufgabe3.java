4743c7323a0721a4e9b0be657e6b4475e09ffa98
WIaimy <github@joebges.com>
2012-11-30 12:10:27 +0100

    update

--- Controlling/src/aufgabe3/Aufgabe3.java
+++ Controlling/src/aufgabe3/Aufgabe3.java
@@ -1,25 +1,19 @@
 package aufgabe3;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Aufgabe3 {
-
-	public class ergebnisObjekt{
-		double wert;
-		int haeufigkeit;
-		public ergebnisObjekt(double start){
-			wert = start;
-		}
-		public double getWert(){
-			return this.wert;
-		}
-	}
 	
-	static List<ergebnisObjekt> umsatz = new ArrayList<ergebnisObjekt>();
-	static List<ergebnisObjekt> kosten = new ArrayList<ergebnisObjekt>();
+	static List<ErgebnisObjekt> umsatz = new ArrayList<ErgebnisObjekt>();
+	static List<ErgebnisObjekt> kosten = new ArrayList<ErgebnisObjekt>();
+	static int counter=0;
+	static FileWriter out = null;
 	
 	public static void main(String[] para) {
 
@@ -46,7 +40,7 @@ public class Aufgabe3 {
 		}
 	}
 
-	static void berechnung(double p, double d, double start, int durchläufe, List<ergebnisObjekt> liste) {
+	static void berechnung(double p, double d, double start, int durchläufe, List<ErgebnisObjekt> liste) {
 		double richtung;
 		
 		for(int j = 0; j < durchläufe; j++){						//x Durchläufe
@@ -65,14 +59,32 @@ public class Aufgabe3 {
 					ergebnis -= d;
 				}
 			}
-			ergebnisObjekt myObject = new Aufgabe3().new ergebnisObjekt(ergebnis);
+			System.out.println("Ergebnis:\t" + ergebnis);
+			ErgebnisObjekt myObject = new ErgebnisObjekt(ergebnis);
 			pruefeObEnthalten(myObject,liste);
+		}
+		
+		try {
+			out = new FileWriter("werte"+(counter++)+".csv");
+			for(ErgebnisObjekt ob : liste){
+				out.append(ob.wert+" ");
+				out.append(ob.haeufigkeit+" ");
+				out.append("\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
-	static void pruefeObEnthalten(ergebnisObjekt ob, List<ergebnisObjekt> liste){
-		for (ergebnisObjekt ob2: liste){								//Liste iterieren
-			if(ob.getWert() == ob2.getWert()){							//Wenn enthalten, häufigkeit hochzählen
+	static void pruefeObEnthalten(ErgebnisObjekt ob, List<ErgebnisObjekt> liste){
+		for (ErgebnisObjekt ob2: liste){								//Liste iterieren
+			if(ob.wert == ob2.wert){									//Wenn enthalten, häufigkeit hochzählen
 				ob2.haeufigkeit++;
 				return;
 			}
---
+++ Controlling/src/aufgabe3/ErgebnisObjekt.java
@@ -0,0 +1,11 @@
+package aufgabe3;
+
+public class ErgebnisObjekt {
+	double wert;
+	int haeufigkeit;
+	
+	public ErgebnisObjekt(double start){
+		wert = start;
+		haeufigkeit = 1;
+	}
+}
